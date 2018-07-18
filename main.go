1b4197416ad502343a14cb5f292b4541c40f82de
Hiu Kwok <hiufkwok@gmail.com>
2018-07-18 15:45:07 +0800

    Parse web file with parameter

--- main.go
+++ main.go
@@ -2,37 +2,20 @@ package main
 
 import (
 	"text/template"
-	"log"
 	"os"
+	"log"
 )
 
-var x int
+var tpl *template.Template
 
 
+func init() {
+	tpl = template.Must(template.ParseFiles("./res/tpl.gohtml"))
+}
 
 func main() {
-
-	tpl, err := template.ParseFiles("./res/tpl.gohtml")
-
-	//Error handling
-	if err!= nil {
-		log.Fatalln(err)
-	}
-	nf, err := os.Create("./temp/index.html")
-
-	if err != nil {
-		log.Println("Error creating file", err)
-	}
-
-	defer nf.Close()
-
-
-
-	//Write to html file instead
-	err = tpl.Execute(nf, nil)
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", 42)
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-
 }
